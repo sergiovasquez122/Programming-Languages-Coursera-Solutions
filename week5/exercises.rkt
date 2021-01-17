9c55b458a0101355e132727a8d8a71af245c34e2
Sergio Vasquez <sergiovasquez122@gmail.com>
2021-01-16 18:38:02 -0800

    added solutions

--- week5/exercises.rkt
+++ week5/exercises.rkt
@@ -8,3 +8,26 @@
                     [(cons (+ (car xs) (car ys))
                            (f (cdr xs) (cdr ys)))]))])
     (f xs ys)))
+
+(define fibonacci
+  (letrec ([f (λ(x y) (cons x (λ() (f y (+ x y)))))])
+    (λ() (f 0 1))))
+
+(define (stream-map f s)
+  (letrec ([g (λ(x)
+                (let* ([pr (x)]
+                 [hd (car pr)]
+                 [tl (cdr pr)])
+            (cons (f hd) (λ() (g tl)))))])
+    (λ()(g s))))
+
+(define (stream-zip s1 s2)
+  (letrec ([g (λ(x y)
+                (let* ([pr1 (x)]
+                       [pr2 (y)]
+                       [hd1 (car pr1)]
+                       [hd2 (car pr2)]
+                       [tl1 (cdr pr1)]
+                       [tl2 (cdr pr2)])
+                  (cons (cons hd1 hd2) (λ()(g tl1 tl2)))))])
+    (λ()(g s1 s2))))
