d1c6dcca4be42978801e76d0871d2c24d40a7261
Sergio Vasquez <sergiovasquez122@gmail.com>
2021-01-23 12:59:45 -0800

    added notes

---
+++ week8/everything_is_an_object.rb
@@ -0,0 +1,24 @@
+# Pure OOP
+# Ruby is fully committed to OOP
+#
+# Simpler, smaller semantics
+# Can call methods on anything
+# - May just get a dynamic undefined method error
+#
+# Almost everything is a method call
+# - Example: 3 + 4
+#
+# All code is methods
+# - All methods you define are part of a class
+# - Top-level methods just added to Object class
+# All objects also have methods
+# - methods
+# - class
+#
+# Can use at run-time to query "what an object can do" and respond accordingly
+# - Called reflection
+#
+# Also useful in the REPL to explore what methods are available
+# - May be quicker than consulting full documentation
+#
+# Another example of "just objects and method calls"
