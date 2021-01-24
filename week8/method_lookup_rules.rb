a02faa727a7cffb476910e49dde0ea2f528d66d1
Sergio Vasquez <sergiovasquez122@gmail.com>
2021-01-24 14:56:06 -0800

    started hw6

---
+++ week8/method_lookup_rules.rb
@@ -0,0 +1,8 @@
+# Dynamic dispatch
+# - Also known as late binding or virtual methods
+# - Call self.m2() in methods m1 deifned in class C can
+# 
+# self maps to some "current" object
+# look up instance variable @x usinb object  bound to self
+# look up class variable @@xx using object bound to self.class
+# look up methods
