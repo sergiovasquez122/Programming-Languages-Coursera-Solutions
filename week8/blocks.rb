4269795233c0e6a42b7b4a6c75aae41601ef5547
Sergio Vasquez <sergiovasquez122@gmail.com>
2021-01-24 12:51:34 -0800

    added notes on blocks

---
+++ week8/blocks.rb
@@ -0,0 +1,5 @@
+# blocks almost just closures
+# normal: easy way to pass anonymous function t o methods for all the usual reasons
+# normal: blocks can take 0 or more arguments
+# normal: blocks use lexical scope: block body uses environment where block was defined
+# can pass 0 or 1 block with any message
