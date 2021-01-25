53cd815f074c6c11f8ac8ea8c466dfe7218e52d1
Sergio Vasquez <sergiovasquez122@gmail.com>
2021-01-25 11:56:07 -0800

    added rotate 180 method

--- week8/hw6assignment.rb
+++ week8/hw6assignment.rb
@@ -1,11 +1,17 @@
-# University of Washington, Programming Languages, Homework 6, hw6runner.rb
-
-# This is the only file you turn in, so do not modify the other files as
-# part of your solution.
-
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-  All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]]]
+  All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+               rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+               [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+               [[0, 0], [0, -1], [0, 1], [0, 2]]],
+               rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+               rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+               rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+               rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), 
+               rotations([[0, 0], [1, 0], [2, 0], [0, 1], [1, 1]]), 
+               [[[0, 0], [-1, 0], [1, 0], [2, 0, [3, 0]]],
+               [[0, 0], [0, -1], [0, 1], [0, 2], [0, 3]]]]
+
   # your enhancements here
   def self.next_piece (board)
     MyPiece.new(All_Pieces.sample, board)
@@ -22,6 +28,12 @@ class MyBoard < Board
     @delay = 500
   end
 
+  def rotate_180
+    if !game_over? and @game.is_running?
+      @current_block.move(0, 0, 2)
+    end
+    draw
+  end
 
   def set_board
     @canvas = TetrisCanvas.new
@@ -46,6 +58,28 @@ class MyTetris < Tetris
     @canvas.place(@board.block_size * @board.num_rows + 3,
                   @board.block_size * @board.num_columns + 6, 24, 80)
     @board.draw
+  end
+
+  def key_bindings  
+    @root.bind('n', proc {self.new_game}) 
+
+    @root.bind('p', proc {self.pause}) 
+
+    @root.bind('q', proc {exitProgram})
+    
+    @root.bind('a', proc {@board.move_left})
+    @root.bind('Left', proc {@board.move_left}) 
+    
+    @root.bind('d', proc {@board.move_right})
+    @root.bind('Right', proc {@board.move_right}) 
+    @root.bind('u', proc{@board.rotate_180})
+    @root.bind('s', proc {@board.rotate_clockwise})
+    @root.bind('Down', proc {@board.rotate_clockwise})
+
+    @root.bind('w', proc {@board.rotate_counter_clockwise})
+    @root.bind('Up', proc {@board.rotate_counter_clockwise}) 
+    
+    @root.bind('space' , proc {@board.drop_all_the_way}) 
   end
 
 end
