1f145e3ab957eddea373e74c8b73e55a4425c414
Sergio Vasquez <sergiovasquez122@gmail.com>
2021-01-25 11:32:17 -0800

    started problem1

--- week8/hw6assignment.rb
+++ week8/hw6assignment.rb
@@ -5,19 +5,47 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
-
+  All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]]]
   # your enhancements here
-
+  def self.next_piece (board)
+    MyPiece.new(All_Pieces.sample, board)
+  end
 end
 
 class MyBoard < Board
   # your enhancements here
+  def initialize (game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
+
+
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
+  end
 
 end
 
 class MyTetris < Tetris
   # your enhancements here
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
 
 end
-
-
